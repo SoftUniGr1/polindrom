dcd11f70e92ba90b899310bc15cc39b64099755a
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-02-22 22:35:46 +0200

    Add files via upload

---
+++ Palindrom 2/Palindrom 2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palindrom_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string word = this.richTextBox1.Text;
+            char[] letters = word.ToCharArray();
+            int lettersLength = word.Length;
+            bool compare = true;
+            for (int i = lettersLength - 1; i >= lettersLength / 2; i--)
+            {
+                if (letters[i] == letters[lettersLength - i - 1])
+                {
+                    compare = true;
+                }
+                else
+                {
+                    compare = false;
+                    break;
+                }
+            }
+            if(compare == true)
+            {
+                MessageBox.Show("Your word is a palindrom");
+            }
+            else
+            {
+                MessageBox.Show("Your word is not a palindrom");
+            }
+
+        }
+
+    }
+}
